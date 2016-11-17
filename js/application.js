0ca5752d5b87888e85ab1371c4f36b82d0c7ee56
Charchit Gupta <charchit26@gmail.com>
2016-11-17 05:04:33 +0000

    transfer from my-space-cloned

---
+++ js/application.js
@@ -0,0 +1,35 @@
+var mainModule = angular.module("mainModule", [ 'ngRoute']);
+
+mainModule.controller("mainCtrlr", function($scope){
+   $scope.boo = (typeof $().emulateTransitionEnd == 'function');
+});
+//Scroll speed maintain
+$.fn.moveIt = function(){
+  var $window = $(window);
+  var instances = [];
+  
+  $(this).each(function(){
+    instances.push(new moveItItem($(this)));
+  });
+  
+  window.onscroll = function(){
+    var scrollTop = $window.scrollTop();
+    instances.forEach(function(inst){
+      inst.update(scrollTop);
+    });
+  }
+}
+
+var moveItItem = function(el){
+  this.el = $(el);
+  this.speed = parseInt(this.el.attr('data-scroll-speed'));
+};
+
+moveItItem.prototype.update = function(scrollTop){
+  var pos = scrollTop / this.speed;
+  this.el.css('transform', 'translateY(' + -pos + 'px)');
+};
+
+$(function(){
+  $('[data-scroll-speed]').moveIt();
+});
